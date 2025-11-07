780d3167fff4a50c999b622392621c37ddcd9556
Baptiste <113362872+Tchoup7790@users.noreply.github.com>
2025-11-07 15:21:56 +0100

    Del output

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from src.utils import save_to_json
 
 if __name__ == "__main__":
     ads = scrape_all_pages(max_pages=4)
-    save_to_json(ads, "./output/ads.json")
+    save_to_json(ads, "./dist/ads.json")
     insert_ads_to_db(ads)
-
